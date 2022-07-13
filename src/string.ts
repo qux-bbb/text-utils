eec94dbea4cb90e0f0b5dd2e774b546f93a2c8aa
qux-bbb <1147635419@qq.com>
2022-07-13 22:59:56 +0800

    adapt multi selections string processing

--- src/string.ts
+++ src/string.ts
@@ -1,41 +1,86 @@
-import { getSelectionString, setSelectionString } from './utils';
+import { window } from 'vscode';
 
 export function reverseString () {
-	let theString = getSelectionString();
-	if (!theString)
-        {return;}
-        
-    const reversed = theString.split('').reverse().join('').replace(/\n\r/g, '\r\n');
+	const editor = window.activeTextEditor;
 
-    setSelectionString(reversed);
+	if (!editor)
+		{return;}
+
+	const document = editor.document;
+	editor.edit(editBuilder => {
+		for (const selection of editor.selections){
+			let theString = document.getText(selection);
+			if (!theString)
+				{return;}
+			const reversed = theString.split('').reverse().join('').replace(/\n\r/g, '\r\n');
+			editBuilder.replace(selection, reversed);
+		}
+	});
 }
 
 export function transformToLowercase () {
-	let theString = getSelectionString();
-	if (!theString)
-        {return;}
-    
-    setSelectionString(theString.toLowerCase());
+	const editor = window.activeTextEditor;
+
+	if (!editor)
+		{return;}
+
+	const document = editor.document;
+	editor.edit(editBuilder => {
+		for (const selection of editor.selections){
+			let theString = document.getText(selection);
+			if (!theString)
+				{return;}
+			editBuilder.replace(selection, theString.toLowerCase());
+		}
+	});
 }
 
 export function transformToUppercase () {
-	let theString = getSelectionString();
-	if (!theString)
-        {return;}
-    
-    setSelectionString(theString.toUpperCase());
+	const editor = window.activeTextEditor;
+
+	if (!editor)
+		{return;}
+
+	const document = editor.document;
+	editor.edit(editBuilder => {
+		for (const selection of editor.selections){
+			let theString = document.getText(selection);
+			if (!theString)
+				{return;}
+			editBuilder.replace(selection, theString.toUpperCase());
+		}
+	});
 }
  
 export function transformToTitleCase () {
-	let theString = getSelectionString();
-	if (!theString)
-        {return;}
+	const editor = window.activeTextEditor;
 
-	let str = theString.toLowerCase().split(' ');
-	for (var i = 0; i < str.length; i++) {
-		str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
-	}
-    const titleString = str.join(' ');
-    
-    setSelectionString(titleString);
+	if (!editor)
+		{return;}
+
+	const document = editor.document;
+	editor.edit(editBuilder => {
+		for (const selection of editor.selections){
+			let theString = document.getText(selection);
+			if (!theString)
+				{return;}
+
+			let titleString = '';
+			if (theString.length == 1)
+				titleString = theString.toUpperCase();
+			else{
+				theString = theString.toLowerCase();
+				const preCharList = [' ', '\r', '\n'];
+				titleString = theString[0].toUpperCase();
+				for (let i = 0; i < theString.length-1; i++) {
+					if (preCharList.includes(theString[i]))
+						titleString += theString[i+1].toUpperCase();
+					else
+						titleString += theString[i+1];
+				}
+			}
+
+			editBuilder.replace(selection, titleString);
+		}
+	});
 }
