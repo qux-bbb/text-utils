e1af8a3c34fd52129fb77ee028fdd6c7b7c0b266
qux-bbb <1147635419@qq.com>
2023-01-21 11:06:07 +0800

    fix sort newline symbol

--- src/sort.ts
+++ src/sort.ts
@@ -1,14 +1,27 @@
 import { getTheString, setTheString } from './utils';
 
+function getNewlineSymbol(theString:string) {
+	let newlineSymbol = '\n';
+
+	if (theString.includes('\r\n')) {
+		newlineSymbol = '\r\n';
+	}else if (theString.includes('\r')) {
+		newlineSymbol = '\r';
+	}
+
+	return newlineSymbol;
+}
+
 export function sortLinesByAlpha () {
 	let [theString, selectFlag] = getTheString();
 	if (!theString)
 		{return;}
-	let theLines = theString.split('\n');
+	let newlineSymbol = getNewlineSymbol(theString);
+	let theLines = theString.split(newlineSymbol);
 
 	theLines.sort();
 
-	let resultString = theLines.join('\n');
+	let resultString = theLines.join(newlineSymbol);
 	setTheString(resultString, selectFlag);
 }
 
@@ -16,11 +29,12 @@ export function sortLinesByReverseAlpha () {
 	let [theString, selectFlag] = getTheString();
 	if (!theString)
 		{return;}
-	let theLines = theString.split('\n');
+	let newlineSymbol = getNewlineSymbol(theString);
+	let theLines = theString.split(newlineSymbol);
 
 	theLines.sort().reverse();
 
-	let resultString = theLines.join('\n');
+	let resultString = theLines.join(newlineSymbol);
 	setTheString(resultString, selectFlag);
 }
 
@@ -28,11 +42,12 @@ export function sortLinesByNum () {
 	let [theString, selectFlag] = getTheString();
 	if (!theString)
 		{return;}
-	let theLines = theString.split('\n');
+	let newlineSymbol = getNewlineSymbol(theString);
+	let theLines = theString.split(newlineSymbol);
 
 	theLines.sort((a, b) => parseInt(a)-parseInt(b));
 
-	let resultString = theLines.join('\n');
+	let resultString = theLines.join(newlineSymbol);
 	setTheString(resultString, selectFlag);
 }
 
@@ -40,10 +55,11 @@ export function sortLinesByReverseNum () {
 	let [theString, selectFlag] = getTheString();
 	if (!theString)
 		{return;}
-	let theLines = theString.split('\n');
+	let newlineSymbol = getNewlineSymbol(theString);
+	let theLines = theString.split(newlineSymbol);
 
 	theLines.sort((a, b) => parseInt(b)-parseInt(a));
 
-	let resultString = theLines.join('\n');
+	let resultString = theLines.join(newlineSymbol);
 	setTheString(resultString, selectFlag);
 }
