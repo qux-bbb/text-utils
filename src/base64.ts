0021331078810baeee0f8565f42d4eebea09124e
qux-bbb <1147635419@qq.com>
2024-09-15 15:05:43 +0800

    add decode_multiline_mode

--- src/base64.ts
+++ src/base64.ts
@@ -1,3 +1,4 @@
+import { workspace } from 'vscode';
 import { getSelectionString, setSelectionString } from './utils';
 
 
@@ -14,11 +15,25 @@ export function base64Decode() {
 	let theString = getSelectionString();
 	if (!theString) { return; }
 
-	let theLines = theString.split('\n');
-	for (let i = 0; i < theLines.length; i++) {
-		theLines[i] = theLines[i] + new Array(theLines[i].length % 4).join('=');
-		theLines[i] = Buffer.from(theLines[i], 'base64').toString();
+	let decode_multiline_mode: number | undefined = workspace.getConfiguration().get('text-utils.base64.decode_multiline_mode');
+	if (decode_multiline_mode === undefined) {
+		console.error('no decode_multiline_mode!');
+		return;
 	}
 
-	setSelectionString(theLines.join('\n'));
+	let resultString = '';
+	if (decode_multiline_mode === 1) {
+		let theLines = theString.split('\n');
+		for (let i = 0; i < theLines.length; i++) {
+			theLines[i] = theLines[i] + new Array(theLines[i].length % 4).join('=');
+			theLines[i] = Buffer.from(theLines[i], 'base64').toString();
+		}
+		resultString = theLines.join('\n');
+	} else if (decode_multiline_mode === 2) {
+		theString = theString.replace('\n', '');
+		theString = theString + new Array(theString.length % 4).join('=');
+		resultString = Buffer.from(theString, 'base64').toString();
+	}
+
+	setSelectionString(resultString);
 }
